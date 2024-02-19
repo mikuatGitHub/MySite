ae8dd829c7d73da59dbd1a12e1bd363a48dae64e
miku <128950322+mikuatGitHub@users.noreply.github.com>
2024-02-19 22:00:26 +0900

    edit-document

--- components/document.js
+++ components/document.js
@@ -3,18 +3,21 @@ import styles from 'styles/document.module.css'
 export default function Document() {
   return (
     <div className={styles.stack}>
-
       <div className={styles.block}>
         <h2>自己紹介</h2>
-        <p>フロントエンドエンジニアに転職したく、プログラミングを独学で勉強しています。</p>
-        <p>ReactとNext.jsを使用してポートフォリオサイトを立ち上げました。</p>
+        <p>
+          ～～～自己紹介文の記述～～～自己紹介文の記述～～～自己紹介文の記述～～～
+          自己紹介文の記述～～～自己紹介文の記述～～～
+          自己紹介文の記述～～～自己紹介文の記述～～～自己紹介文の記述～～～
+        </p>
       </div>
 
       <div className={styles.block}>
         <h2>経歴</h2>
-        <p>大学では環境工学を専攻。現職ではネットワーク系の業務をしています。</p>
+        <p>
+          ～～～経歴の記述～～～経歴の記述～～～経歴の記述～～～経歴の記述～～～経歴の記述～～～経歴の記述～～～経歴の記述～～～経歴の記述～～～経歴の記述～～～経歴の記述～～～
+        </p>
       </div>
-
     </div>
-  )
+  );
 }
